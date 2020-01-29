f06fb69a40d403c5069d5d077f7b519f6c7117ed
aravindhalahalli <aravindhalahalli.81@gmail.com>
2020-01-30 02:55:39 +0530

    added all cities details to json file

--- src/components/tourData.js
+++ src/components/tourData.js
@@ -97,146 +97,146 @@ export const tourData = [
   },
   {
     id: 13,
-    city: "Kalaburgi",
-    img: "https://live.staticflickr.com/7145/6754082367_481db64122_b.jpg",
-    name: "Kalaburgi, Shri Sharanbasaveshwar Temple",
+    city: "Shivamogga",
+    img: "https://lh5.googleusercontent.com/-0gt0_lvhfSk/XZwfTiiXS4I/AAAAAAAAKxk/cArXgprm4mUfcHUvrxaZAPJbO34zOqh1gCLIBGAYYCw/w296-h202-n-k-no/",
+    name: "Shivamogga, Jog Falls View Point",
     info:
-      "Sharana Basaveshwara Temple is a shrine at Kalburgi (Gulbarga), an ancient town in the ... Sajjike (the cover used for keeping the Linga) made in Sandalwood and used by Sharanabasaveshwara.people, particularly farmers from the region."
+      "Jog falls is Awsome place in rainy session must visit once a year"
   },
   {
     id: 14,
-    city: "Kalaburgi",
-    img: "https://live.staticflickr.com/7145/6754082367_481db64122_b.jpg",
-    name: "Kalaburgi, Shri Sharanbasaveshwar Temple",
+    city: "Ramanagara",
+    img: "https://lh3.googleusercontent.com/-SmbxX4iPo8A/WyjAhwk0I3I/AAAAAAABJWU/OiXcSrIUagcNkle-5uBMqSxQsuFcsA6UgCLIBGAYYCw/w296-h202-n-k-no/",
+    name: "Ramanagara, Ramdevara Hill",
     info:
-      "Sharana Basaveshwara Temple is a shrine at Kalburgi (Gulbarga), an ancient town in the ... Sajjike (the cover used for keeping the Linga) made in Sandalwood and used by Sharanabasaveshwara.people, particularly farmers from the region."
+      "Mountain peak"
   },
   {
     id: 15,
-    city: "Kalaburgi",
-    img: "https://live.staticflickr.com/7145/6754082367_481db64122_b.jpg",
-    name: "Kalaburgi, Shri Sharanbasaveshwar Temple",
+    city: "Chikkaballapur",
+    img: "https://lh3.googleusercontent.com/-Q92xnF9HC_M/VvY7hMWhajI/AAAAAAAAMyE/Ec45G1Auj28YWHV0-pLmLCkng17QLSapgCLIBGAYYCw/w296-h202-n-k-no/",
+    name: "Chikkaballapur, Skandagiri",
     info:
-      "Sharana Basaveshwara Temple is a shrine at Kalburgi (Gulbarga), an ancient town in the ... Sajjike (the cover used for keeping the Linga) made in Sandalwood and used by Sharanabasaveshwara.people, particularly farmers from the region."
+      "Hill with fort ruins & a hiking trail"
   },
   {
     id: 16,
-    city: "Kalaburgi",
-    img: "https://live.staticflickr.com/7145/6754082367_481db64122_b.jpg",
-    name: "Kalaburgi, Shri Sharanbasaveshwar Temple",
+    city: "Chitradurga",
+    img: "https://t3.gstatic.com/images?q=tbn:ANd9GcSy29eY1H6xXHIyBWumeOFnpCM-uwxAj2rQN_T_2JZuSrv8Lch312KcecXqLvxKtixfCUU63v5ofjsQDg",
+    name: "Chitradurga, Chitradurga Fort",
     info:
-      "Sharana Basaveshwara Temple is a shrine at Kalburgi (Gulbarga), an ancient town in the ... Sajjike (the cover used for keeping the Linga) made in Sandalwood and used by Sharanabasaveshwara.people, particularly farmers from the region."
+      "Chitradurga Fort or as the British called it Chitaldoorg, is a fortification that straddles several hills and a peak overlooking a flat valley in the Chitradurga District, Karnataka, India. The fort's name Chitrakaldurga, which means 'picturesque fort' in Kannada, is the namesake of the town Chitradurga and its administrative district."
   },
   {
     id: 17,
-    city: "Kalaburgi",
-    img: "https://live.staticflickr.com/7145/6754082367_481db64122_b.jpg",
-    name: "Kalaburgi, Shri Sharanbasaveshwar Temple",
+    city: "Davanagere",
+    img: "https://lh5.googleusercontent.com/-znODRqmfFzc/XKH3DLu2_FI/AAAAAAAAj4Q/L0mAJHQcU1EeYvUeywf3MAAAWXEizsgWwCLIBGAYYCw/w296-h202-n-k-no/",
+    name: "Davanagere, Rocky hill shaped like a face",
     info:
-      "Sharana Basaveshwara Temple is a shrine at Kalburgi (Gulbarga), an ancient town in the ... Sajjike (the cover used for keeping the Linga) made in Sandalwood and used by Sharanabasaveshwara.people, particularly farmers from the region."
+      "Kundavada Kere is a lake in Davanagere city, Karnataka state, India."
   },
   {
     id: 18,
-    city: "Kalaburgi",
-    img: "https://live.staticflickr.com/7145/6754082367_481db64122_b.jpg",
-    name: "Kalaburgi, Shri Sharanbasaveshwar Temple",
+    city: "Ballari",
+    img: "https://lh5.googleusercontent.com/-pUTis3g-Ky4/XgSyrQ7gRYI/AAAAAAAADsM/4MalN_bDgEEe-rEzdiXsEnhEnMOTFw4PwCLIBGAYYCw/w296-h202-n-k-no/",
+    name: "Ballari, Kumbara Gudda",
     info:
-      "Sharana Basaveshwara Temple is a shrine at Kalburgi (Gulbarga), an ancient town in the ... Sajjike (the cover used for keeping the Linga) made in Sandalwood and used by Sharanabasaveshwara.people, particularly farmers from the region."
+      "Rocky hill shaped like a face."
   },
   {
     id: 19,
-    city: "Kalaburgi",
-    img: "https://live.staticflickr.com/7145/6754082367_481db64122_b.jpg",
-    name: "Kalaburgi, Shri Sharanbasaveshwar Temple",
+    city: "Bidar",
+    img: "https://t2.gstatic.com/images?q=tbn:ANd9GcSr1oVRvg7kx-qJGbZ1ZFjuzae7LsXLzP19EnrbLEWKmn725kwixQ3SjmeIUXAOhH5dLSgbUBG7rm9E3g",
+    name: "Bidar, Bidar Fort",
     info:
-      "Sharana Basaveshwara Temple is a shrine at Kalburgi (Gulbarga), an ancient town in the ... Sajjike (the cover used for keeping the Linga) made in Sandalwood and used by Sharanabasaveshwara.people, particularly farmers from the region."
+      "Bidar Fort is a fort situated in the Bidar, Karnataka, India. The fort, the city and the district are all affixed with the name Bidar. Sultan Alla-Ud Din Bahman of the Bahmanid Dynasty shifted his capital from Gulbarga to Bidar in 1427 and built his fort along with a number of Islamic monuments. There are over 30 monuments inside Bidar fort"
   },
   {
     id: 20,
-    city: "Kalaburgi",
-    img: "https://live.staticflickr.com/7145/6754082367_481db64122_b.jpg",
-    name: "Kalaburgi, Shri Sharanbasaveshwar Temple",
+    city: "Koppal",
+    img: "https://cdn.s3waas.gov.in/s36cd67d9b6f0150c77bda2eda01ae484c/uploads/bfi_thumb/2018072542-ntmx4uev6lzr2mf8by0jah0xtlxwbeva7ql72b14ig.jpg",
+    name: "Koppal, Mahadeva Temple",
     info:
       "Sharana Basaveshwara Temple is a shrine at Kalburgi (Gulbarga), an ancient town in the ... Sajjike (the cover used for keeping the Linga) made in Sandalwood and used by Sharanabasaveshwara.people, particularly farmers from the region."
   },
   {
     id: 21,
-    city: "Kalaburgi",
-    img: "https://live.staticflickr.com/7145/6754082367_481db64122_b.jpg",
-    name: "Kalaburgi, Shri Sharanbasaveshwar Temple",
+    city: "Raichur",
+    img: "https://c1.hiqcdn.com/customcdn/350x350/uploadimages/travel/shareiq_1328766382.281522.jpg",
+    name: "Raichur, Lake of Raichur",
     info:
-      "Sharana Basaveshwara Temple is a shrine at Kalburgi (Gulbarga), an ancient town in the ... Sajjike (the cover used for keeping the Linga) made in Sandalwood and used by Sharanabasaveshwara.people, particularly farmers from the region."
+      "built in 1112 CE. Soapstone was found in abundance in this regions.The first temple to be built from this material…"
   },
   {
     id: 22,
-    city: "Kalaburgi",
-    img: "https://live.staticflickr.com/7145/6754082367_481db64122_b.jpg",
-    name: "Kalaburgi, Shri Sharanbasaveshwar Temple",
+    city: "Yadgir",
+    img: "https://4.bp.blogspot.com/-6wE5ZcAvW24/UWgZCBxODCI/AAAAAAAASbI/TdEZ6fkYIy0/s1600/27+Yadgiri+hill+fort.jpg",
+    name: "Yadgir, Yadagiri Fort",
     info:
       "Sharana Basaveshwara Temple is a shrine at Kalburgi (Gulbarga), an ancient town in the ... Sajjike (the cover used for keeping the Linga) made in Sandalwood and used by Sharanabasaveshwara.people, particularly farmers from the region."
   },
   {
     id: 23,
-    city: "Kalaburgi",
-    img: "https://live.staticflickr.com/7145/6754082367_481db64122_b.jpg",
-    name: "Kalaburgi, Shri Sharanbasaveshwar Temple",
+    city: "Bagalkot",
+    img: "https://static.wixstatic.com/media/ba6f17_6f7adfadf3357da3201496b57a1541be.jpg/v1/fill/w_600,h_429,al_c,lg_1,q_80/ba6f17_6f7adfadf3357da3201496b57a1541be.webp",
+    name: "Bagalkot, Kudalasangama",
     info:
-      "Sharana Basaveshwara Temple is a shrine at Kalburgi (Gulbarga), an ancient town in the ... Sajjike (the cover used for keeping the Linga) made in Sandalwood and used by Sharanabasaveshwara.people, particularly farmers from the region."
+      "Kudalasangama is present 15 kilometers away from Almati Dam. Basavanna Bagewadi, the birthplace of Basavanna the social and religious reformer, is located here. It has the Samadhi of Basavanna, known as the Aikya Mantapa.andThe Kudalasangama is situated in Bagalkot district. The place has Shivling and known as Samadhi place of Saint Basaveshwara. This place is situated over sangam of three rivers (Krishna, Tungabhadra, )."
   },
   {
     id: 24,
-    city: "Kalaburgi",
-    img: "https://live.staticflickr.com/7145/6754082367_481db64122_b.jpg",
-    name: "Kalaburgi, Shri Sharanbasaveshwar Temple",
+    city: "Belagavi",
+    img: "https://lh4.googleusercontent.com/-UfyNbWCQ7-Q/XVjiQHJc7xI/AAAAAAAADmw/D_ZyzoTKRhcFWGQx9axuyTaQGDly66m-gCLIBGAYYCw/w296-h202-n-k-no/",
+    name: "Belagavi, Rajhunsgad Fort",
     info:
       "Sharana Basaveshwara Temple is a shrine at Kalburgi (Gulbarga), an ancient town in the ... Sajjike (the cover used for keeping the Linga) made in Sandalwood and used by Sharanabasaveshwara.people, particularly farmers from the region."
   },
   {
     id: 25,
-    city: "Kalaburgi",
-    img: "https://live.staticflickr.com/7145/6754082367_481db64122_b.jpg",
-    name: "Kalaburgi, Shri Sharanbasaveshwar Temple",
+    city: "Vijayapura",
+    img: "https://t3.gstatic.com/images?q=tbn:ANd9GcTSPb-9JDq42hld6CVJLyuvgwaYJYbT01-AdGj2SMTmCLzmpvw0brpEs7Cg-jbUDv48MlfTdP_YPw0zOg",
+    name: "Vijayapura, Gol Gumbaz",
     info:
-      "Sharana Basaveshwara Temple is a shrine at Kalburgi (Gulbarga), an ancient town in the ... Sajjike (the cover used for keeping the Linga) made in Sandalwood and used by Sharanabasaveshwara.people, particularly farmers from the region."
+      "Gol Gumbaz at Bijapur is the mausoleum of king Muhammad Adil Shah, Adil Shah Dynasty. Construction of the tomb, located in Vijayapura, Karnataka, India, was started in 1626 and completed in 1656. The name is based on Gola gummata derived from Gol Gombadh meaning circular dome. It follows the style of Indo-islamic architecture. Even a slight whisper by someone standing in this gallery can be heard everywhere in the gallery, and if somebody claps, the echo can be heard several time."
   },
   {
     id: 26,
-    city: "Kalaburgi",
-    img: "https://live.staticflickr.com/7145/6754082367_481db64122_b.jpg",
-    name: "Kalaburgi, Shri Sharanbasaveshwar Temple",
+    city: "Dharwad",
+    img: "https://lh3.googleusercontent.com/-DeONEFR5sII/WmIpkP6HL_I/AAAAAAAAFh8/WkmNd_hTdfI46j8cVRBbcOuH_HZ_bolNgCLIBGAYYCw/w296-h202-n-k-no/",
+    name: "Dharwad, Kargill Sthupa",
     info:
-      "Sharana Basaveshwara Temple is a shrine at Kalburgi (Gulbarga), an ancient town in the ... Sajjike (the cover used for keeping the Linga) made in Sandalwood and used by Sharanabasaveshwara.people, particularly farmers from the region."
+      "Memorial to Kargil War soldiers."
   },
   {
     id: 27,
-    city: "Kalaburgi",
-    img: "https://live.staticflickr.com/7145/6754082367_481db64122_b.jpg",
-    name: "Kalaburgi, Shri Sharanbasaveshwar Temple",
+    city: "Gadag",
+    img: "https://lh4.googleusercontent.com/-dhEhJzG9Kcc/XcZIG5QEaLI/AAAAAAAACXY/B4qfzLIB4IM4nyEFCA6oD2Ow6xK5KsW3ACLIBGAYYCw/w296-h202-n-k-no/",
+    name: "Gadag, Basaveshwara statue",
     info:
-      "Sharana Basaveshwara Temple is a shrine at Kalburgi (Gulbarga), an ancient town in the ... Sajjike (the cover used for keeping the Linga) made in Sandalwood and used by Sharanabasaveshwara.people, particularly farmers from the region."
+      "Magnificent statue the environment is chilled but no good place to eat"
   },
   {
     id: 28,
-    city: "Kalaburgi",
-    img: "https://live.staticflickr.com/7145/6754082367_481db64122_b.jpg",
-    name: "Kalaburgi, Shri Sharanbasaveshwar Temple",
+    city: "Haveri",
+    img: "https://lh5.googleusercontent.com/-mB4uhplj7VI/XKT4G-yMxRI/AAAAAAAACrI/73FJF_xTa6IJMhmJTbWPRIBhZczLtyogACLIBGAYYCw/w296-h202-n-k-no/",
+    name: "Haveri, Mukteshwara Temple",
     info:
-      "Sharana Basaveshwara Temple is a shrine at Kalburgi (Gulbarga), an ancient town in the ... Sajjike (the cover used for keeping the Linga) made in Sandalwood and used by Sharanabasaveshwara.people, particularly farmers from the region."
+      "Good place and awesome work of art in stone."
   },
   {
     id: 29,
-    city: "Kalaburgi",
-    img: "https://live.staticflickr.com/7145/6754082367_481db64122_b.jpg",
-    name: "Kalaburgi, Shri Sharanbasaveshwar Temple",
+    city: "Uttara Kannada",
+    img: "https://lh4.googleusercontent.com/proxy/ZYQYIZSHfHPlj_KBu0nfNmZEO0hnQv1rF7hjOmfIrSzCVHzIaCl2_ekfBSIsvMWo7Zaai-A73pxuLk59UlRv9sbabO74gEZLN-naErZ57vxkZ7kM53sUxbVvMqStnOdVBA8__A-raWnENSlcklX8SPm3ss43-hRfp80crnyQ_Zfe=w296-h202-n-k-no",
+    name: "Uttara Kannada, Om Beach",
     info:
-      "Sharana Basaveshwara Temple is a shrine at Kalburgi (Gulbarga), an ancient town in the ... Sajjike (the cover used for keeping the Linga) made in Sandalwood and used by Sharanabasaveshwara.people, particularly farmers from the region."
+      "Natural shoreline in a remote holy town."
   },
   {
     id: 30,
-    city: "Kalaburgi",
-    img: "https://live.staticflickr.com/7145/6754082367_481db64122_b.jpg",
-    name: "Kalaburgi, Shri Sharanbasaveshwar Temple",
+    city: "Hubbali",
+    img: "https://lh4.googleusercontent.com/-jBtlGJx1BB0/WfcuMu2JtGI/AAAAAAAAM1M/I_FfqqWzGMUkmLaSKKvanz3T7PBjeS_rQCLIBGAYYCw/w296-h202-n-k-no/",
+    name: "Hubbali, Nrupatunga Betta",
     info:
-      "Sharana Basaveshwara Temple is a shrine at Kalburgi (Gulbarga), an ancient town in the ... Sajjike (the cover used for keeping the Linga) made in Sandalwood and used by Sharanabasaveshwara.people, particularly farmers from the region."
+      "Lush hill with a playground & eatery."
   }
 ];
